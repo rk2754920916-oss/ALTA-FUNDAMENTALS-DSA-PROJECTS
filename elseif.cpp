ac378e9588072a927caf35e2ba97f1db1475f3b4
rakeshkumar_19 <rk2754920916@gmail.com>
2026-09-17 11:01:33 -0700

    Add files via upload
    Day 6/111 ALTA BASE - DSA SHEET

---
+++ elseif.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+       
+    int a, b, c;
+
+    cout << "a = ";
+    cin >> a;
+
+    cout << "b = ";
+    cin >> b;
+
+    cout << "c = ";
+    cin >> c;
+  
+ 
+   
+
+    if (a > b) {
+        if (a > c) {
+            cout << "a";
+        }
+        else {
+        cout << "c";
+        }
+    }
+    else {
+        if (b > c){
+            cout << "b";
+        }
+        else {
+            cout << "c";
+
+        }
+
+    }
+    return 0;
+}
